ebeda2358394b75f29f53a3b652fbf0e3eb3e56c
ha3-tech <69754051+ha3-tech@users.noreply.github.com>
2024-09-12 16:17:40 +0530

    change route

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://sarthi-frontend-phi.vercel.app/",
+    origin: "https://sarthi-frontend-phi.vercel.app",
   })
 );
 
